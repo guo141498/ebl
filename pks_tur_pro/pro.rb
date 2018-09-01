22578e6b18f24ee5ff6976e08bd0077bf42d7a9b
guo141498 <34337440+guo141498@users.noreply.github.com>
2018-09-01 16:11:41 +0800

    Update pro.rb

--- pks_tur_pro/pro.rb
+++ pks_tur_pro/pro.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 #---------------update
+system("g++ -std=c++11 nlopt_turm_mwmix_309_probability.cc matrix.cc -O2 -o 3.out ")
 require '~/.scripts/pool.rb'
 def f(i)
 	system("echo #{i}")
-	system("g++ -std=c++11 nlopt_turm_mwmix_232_probability.cc matrix.cc -O2 -o 3.out ")
+	
 	system("cat work.sh |\
 			 sed \"/^i=/c \i=#{i}\ \" >  work_#{i}.sh")
 	system("chmod +x work_#{i}.sh")
